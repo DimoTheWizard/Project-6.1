f29b0e72857ffed037d2d3807143c7654ef53d05
Dimitri Vastenhout <dimitri.vastenhout@gmail.com>
2023-02-09 15:39:20 +0100

    made API base

---
+++ Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace API.Models
+{
+    public class Transaction
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("transaction")]
+        [JsonPropertyName("transaction")]
+        public string transaction { get; set; } = null;
+    }
+}
