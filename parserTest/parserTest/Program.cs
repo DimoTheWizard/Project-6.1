d95cca65d2713a800cbd197201e6b7d30440fd73
DavidHlavacek <davi.hlav@gmail.com>
2023-02-16 01:39:32 +0100

    Further developping parserTest branch with correct packages | TODO: Add SharpMt940Lib.Core NuGet Package

--- parserTest/parserTest/Program.cs
+++ parserTest/parserTest/Program.cs
@@ -6,7 +6,8 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Globalization;
 using System.Text.Json;
-
+using Raptorious.SharpMt940Lib;
+//ADD NUGET PACKAGE SharpMt940Lib.Core 1.0.2
 
 namespace MT940Parser
 {
@@ -17,125 +18,24 @@ namespace MT940Parser
         static void Main(string[] args)
         {
             //RAW MT940 OBJECT
-            MT940 mt940 = new MT940(FILENAME);
+            var start = new Raptorious.SharpMt940Lib.Mt940Format.Separator("STARTUMSE");
+            var end = new Raptorious.SharpMt940Lib.Mt940Format.Separator("-");
+            var genericFomat = new Raptorious.SharpMt940Lib.Mt940Format.GenericFormat(start, end);
+            var parsed = Mt940Parser.Parse(genericFomat, FILENAME, CultureInfo.CurrentCulture);
+            var output = JsonSerializer.Serialize(parsed);
             
             //JSON OBJECT
-            var output = "";
-            if(mt940.Checker()) {
-                output = JsonSerializer.Serialize(mt940);
-            } else {
-                output = "invalid.";
-            }
+            // var output = "";
+            // if(mt940.Checker()) {
+            //     output = JsonSerializer.Serialize(mt940);
+            // } else {
+            //     output = "invalid.";
+            // }
             // string output = JsonConvert.SerializeObject(mt940);
 
             //TEST
             Console.WriteLine(output);
 
         }
-    }
-    public class MT940
-    {
-        const string TAG_PATTERN = @"^:(?'tag'[^:]+):(?'value'.*)";
-
-        const int requiredFields = 5;
-
-        public string transactionReferenceNumber { get; set; }  // 20 *
-                                                                // 21
-        public string accountIdentification { get; set; }               // 25 *
-        public string messageIndexTotal { get; set; }           // 28C *
-        public Currency openingBalance { get; set; }            // 60a * 
-        public string firstTransaction { get; set; }            // 61
-        public string secondTransaction { get; set; }           // 61    
-                                                                 // 86                
-        public Currency closingBalance { get; set; }            // 62a *
-                                                                //64
-                                                                // 65                
-                                                                // 86
-        public Boolean valid;
-
-        public MT940(string filename)
-        {
-            StreamReader reader = new StreamReader(filename);
-            string line = "";
-            int transactionCount = 0;
-
-            while ((line = reader.ReadLine()) != null)
-            {
-                if (line.StartsWith(":"))
-                {
-                    Match match = Regex.Match(line, TAG_PATTERN);
-                    string tag = match.Groups["tag"].Value;
-                    string value = match.Groups["value"].Value;
-
-                    switch (tag)
-                    {
-                        case "20":
-                            transactionReferenceNumber = value;
-                            break;
-
-                        case "25":
-                            accountIdentification = value;
-                            break;
-
-                        case "28C":
-                            messageIndexTotal = value;
-                            break;
-
-                        case "60F":
-                            openingBalance = new Currency(value);
-                            break;
-                        case "61":
-                            if (++transactionCount == 1)
-                            {
-                                firstTransaction = value;
-                            }
-                            else
-                            {
-                                secondTransaction = value;
-                            }
-                            break;
-
-                        case "62F":
-                            closingBalance = new Currency(value);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-        }
-
-        public Boolean Checker() {
-            if(this.transactionReferenceNumber == null || this.accountIdentification == null || this.messageIndexTotal == null || this.openingBalance == null || this.closingBalance == null) {
-                return false;
-            }
-            return true;
-        }
-
-    }
-    public class Currency
-    {
-        const string BALANCE_PATTERN = @"^(?'credit_debit'.)(?'date'.{6})(?'country_code'.{3})(?'amount'.*)";
-        static CultureInfo culture = CultureInfo.GetCultureInfoByIetfLanguageTag("da");
-
-        public DateTime date { get; set; }
-        public string currencyCode { get; set; }
-        public decimal amount { get; set; }
-
-        public Currency(string input)
-        {
-            Match match = Regex.Match(input, BALANCE_PATTERN);
-
-            string credit_debit = match.Groups["credit_debit"].Value;
-
-            string dateStr = match.Groups["date"].Value;
-            date = DateTime.ParseExact(dateStr, "yyMMdd", CultureInfo.InvariantCulture);
-
-            currencyCode = match.Groups["country_code"].Value;
-
-            string amountStr = match.Groups["amount"].Value;
-            amount = decimal.Parse(amountStr, culture);
-            amount *= credit_debit == "D" ? -1 : 1;
-        }
-    }
+    }   
 }
