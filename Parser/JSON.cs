51fbe7cd9d094b1bb79dd731ab9ebfaaaedc58d4
DavidHlavacek <davi.hlav@gmail.com>
2023-02-21 14:03:04 +0100

    Created a proper Parser structure project

---
+++ Parser/JSON.cs
@@ -0,0 +1,17 @@
+namespace Parser 
+{
+    class JSON
+    {
+        private var json;
+
+        public void JSON(var json)
+        {
+            this.json = json;
+        }
+
+        public var getJson()
+        {
+            return this.json;
+        }
+    }
+}
