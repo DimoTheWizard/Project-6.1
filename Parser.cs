1bbf11826dbac081f9e2f0d0a3fbc90362fe3b07
Dimitri Vastenhout <dimitri.vastenhout@gmail.com>
2023-02-21 13:07:53 +0100

    added parser and api

---
+++ Parser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Text.Json;
+using Raptorious.SharpMt940Lib;
+//ADD NUGET PACKAGE SharpMt940Lib.Core 1.0.2
+
+namespace MT940Parser
+{
+    class Parser
+    {
+        static void Parse(fileName)
+        {
+            //RAW MT940 OBJECT
+            var start = new Raptorious.SharpMt940Lib.Mt940Format.Separator("STARTUMSE");
+            var end = new Raptorious.SharpMt940Lib.Mt940Format.Separator("-");
+            var genericFomat = new Raptorious.SharpMt940Lib.Mt940Format.GenericFormat(start, end);
+            ICollection<CustomerStatementMessage> parsed = null;
+            try {
+                parsed = Mt940Parser.Parse(genericFomat, fileName, CultureInfo.CurrentCulture);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid.");
+            }
+
+            var output = "";
+            if(Raptorious.SharpMt940Lib.Mt940Parser.valid) {
+                output = JsonSerializer.Serialize(parsed);
+            } else {    
+                output = "Invalid.";
+            }
+                        
+            //JSON OBJECT
+            // var output = "";
+            // if(mt940.Checker()) {
+            //     output = JsonSerializer.Serialize(mt940);
+            // } else {
+            //     output = "invalid.";
+            // }
+            // string output = JsonConvert.SerializeObject(mt940);
+
+            //TEST
+            Console.WriteLine(output);
+
+        }
+    }   
+}
