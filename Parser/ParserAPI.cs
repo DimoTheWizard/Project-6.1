7b366aa9df8411abdf88a6cff15cf699dddb6c99
DavidHlavacek <davi.hlav@gmail.com>
2023-03-07 17:16:13 +0100

    Uncommented main Parser file

--- Parser/ParserAPI.cs
+++ Parser/ParserAPI.cs
@@ -1,54 +1,53 @@
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.IO;
-//using System.Text.RegularExpressions;
-//using System.Globalization;
-//using System.Text.Json;
-//using Raptorious.SharpMt940Lib;
-////ADD NUGET PACKAGE SharpMt940Lib.Core 1.0.2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Text.Json;
+using Raptorious.SharpMt940Lib;
 
-//namespace Parser
-//{
-//    class ParserAPI
-//    {
-//        MT940 mt940;
-//        JSON json;
-//        public ParserAPI(MT940 mt940)
-//        {
-//            this.mt940 = mt940;
-//        }
+namespace Parser
+{
+   class ParserAPI
+   {
+       MT940 mt940;
+       JSON json;
+       public ParserAPI(MT940 mt940)
+       {
+           this.mt940 = mt940;
+       }
 
-//        public bool parseMT940_TO_JSON()
-//        {
-//            object output = "";
-//            var start = new Raptorious.SharpMt940Lib.Mt940Format.Separator("STARTUMSE");
-//            var end = new Raptorious.SharpMt940Lib.Mt940Format.Separator("-");
-//            var genericFomat = new Raptorious.SharpMt940Lib.Mt940Format.GenericFormat(start, end);
-//            ICollection<CustomerStatementMessage> parsed = null;
-//            try
-//            {
-//                parsed = Mt940Parser.Parse(genericFomat, this.mt940.getMT940(), CultureInfo.CurrentCulture);
-//            }
-//            catch
-//            {
-//                return false;
-//            }
+       public bool parseMT940_TO_JSON()
+       {
+           object output = "";
+           var start = new Raptorious.SharpMt940Lib.Mt940Format.Separator("STARTUMSE");
+           var end = new Raptorious.SharpMt940Lib.Mt940Format.Separator("-");
+           var genericFomat = new Raptorious.SharpMt940Lib.Mt940Format.GenericFormat(start, end);
+           ICollection<CustomerStatementMessage> parsed = null;
+           try
+           {
+               parsed = Mt940Parser.Parse(genericFomat, this.mt940.getMT940(), CultureInfo.CurrentCulture);
+           }
+           catch
+           {
+               return false;
+           }
 
-//            if(Mt940Parser.valid) {
-//                output = JsonSerializer.Serialize(parsed);
-//                JSON json = new JSON(output);
-//                this.json = json;
-//                return true;
-//            } 
+           if(Mt940Parser.valid) {
+               output = JsonSerializer.Serialize(parsed);
+               JSON json = new JSON(output);
+               this.json = json;
+               return true;
+           } 
             
-//            return false;
-//        }
+           return false;
+       }
 
-//        public string getJSON() 
-//        {
-//            return this.json.getJson();
-//        }
-//    }   
-//}
+       public string getJSON() 
+       {
+           return this.json.getJson();
+       }
+   }   
+}
