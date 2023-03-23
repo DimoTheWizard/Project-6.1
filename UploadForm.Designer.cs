7c28d05a824d9bc0706de8acffb2c17355b2118c
Andreikova <112638712+Andreikova@users.noreply.github.com>
2023-03-23 15:02:13 +0100

    File upload
    Not complete - It should accept any MT940 file and should allow multiple files to be uploaded at once and it records the path of the file

---
+++ UploadForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Sports_Accounting
+{
+    partial class UploadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Upload = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Upload
+            // 
+            this.Upload.Location = new System.Drawing.Point(122, 283);
+            this.Upload.Name = "Upload";
+            this.Upload.Size = new System.Drawing.Size(100, 30);
+            this.Upload.TabIndex = 0;
+            this.Upload.Text = "Upload File(s)";
+            this.Upload.UseVisualStyleBackColor = true;
+            this.Upload.Click += new System.EventHandler(this.Upload_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // UploadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.Upload);
+            this.MaximumSize = new System.Drawing.Size(1600, 900);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "UploadForm";
+            this.Text = "Upload file";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Upload;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
---
+++ UploadForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Sports_Accounting
+{
+    public partial class UploadForm : Form
+    {
+        public UploadForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Upload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "mt940 files (*.mt940)|*mt940|All files (*.*)|*.*"; //Files types to be allowed
+            dialog.Multiselect = true; //Allow more than 1 file to be uploaded
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                String path = dialog.FileName;
+            }
+
+        }
+    }
+}
