6181aa0f8c9bc1364b4695afbeb5cda9f9c7f79c
Corvin Wittmaack <corvin.wittmaack@student.nhlstenden.com>
2023-03-29 22:45:06 +0200

    added views and sp

---
+++ Programmability/Procedures/dbo.check_work.sql
@@ -0,0 +1,10 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+
+CREATE PROCEDURE [dbo].[check_work] 
+@Employee NVARCHAR(50)
+AS
+BEGIN
+	SELECT staff_name FROM	internal_work WHERE	staff_name = @Employee
+END
+GO
---
+++ Programmability/Procedures/dbo.get_transactions.sql
@@ -0,0 +1,9 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+
+CREATE PROCEDURE [dbo].[get_transactions] @ID INT
+AS
+BEGIN
+	SELECT transaction_id FROM dbo.user_transactions WHERE customer_id = @ID;
+END
+GO
